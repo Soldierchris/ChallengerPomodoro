9486c5534d4c765cf76f44bea364f07496ba44e4
Christian <ccastro@corpuscorp.com>
2025-02-12 11:18:06 -0300

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
-import App from './App.jsx'
+
+import './index.css'
 import PomodoroTimer from './components/PomodoroTimer.jsx'
 
 createRoot(document.getElementById('root')).render(
